0fc885069e069c2ca0ef1ae402cb35d52a69067c
032nnxkitty <aguznenkovv@yandex.ru>
2023-04-15 21:41:51 +0200

    Create base settings UI

--- DogeExplorerApp_iOS/Modules/SettingsModule/SettingsPresenter.swift
+++ DogeExplorerApp_iOS/Modules/SettingsModule/SettingsPresenter.swift
@@ -9,6 +9,8 @@ import Foundation
 
 protocol SettingsPresenter {
     init(view: SettingsView)
+    func getNumberOfSection() -> Int
+    func getNumberOfRows(in section: Int) -> Int
 }
 
 final class SettingsPresenterImp: SettingsPresenter {
@@ -16,5 +18,13 @@ final class SettingsPresenterImp: SettingsPresenter {
     
     init(view: SettingsView) {
         self.view = view
+    }
+    
+    func getNumberOfSection() -> Int {
+        return 0
+    }
+    
+    func getNumberOfRows(in section: Int) -> Int {
+        return 0
     }
 }
--- DogeExplorerApp_iOS/Modules/SettingsModule/SettingsViewController.swift
+++ DogeExplorerApp_iOS/Modules/SettingsModule/SettingsViewController.swift
@@ -12,14 +12,80 @@ protocol SettingsView: AnyObject {
 }
 
 final class SettingsViewController: UIViewController {
-    public var presenter: SettingsPresenter?
+    public var presenter: SettingsPresenter!
     
+    // MARK: - UI Elements
+    private let settingsTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "settingsCell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
+    
+    private lazy var themeSegmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["Dark", "Light", "Device"])
+        segmentedControl.addTarget(self, action: #selector(themeChanged), for: .valueChanged)
+        return segmentedControl
+    }()
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureViewAppearance()
+        configureSettingsTableView()
     }
 }
 
+// MARK: - Private Methods
+private extension SettingsViewController {
+    func configureViewAppearance() {
+        title = "Settings"
+        view.backgroundColor = .systemBackground
+    }
+    
+    func configureSettingsTableView() {
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
+        
+        view.addSubview(settingsTableView)
+        NSLayoutConstraint.activate([
+            settingsTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            settingsTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            settingsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - Actions
+@objc private extension SettingsViewController {
+    func themeChanged(_ sender: UISegmentedControl) {
+       
+    }
+}
+
+// MARK: - SettingsView Protocol
 extension SettingsViewController: SettingsView {
+    
+}
+// MARK: - UITableViewDataSource
+extension SettingsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return presenter.getNumberOfSection()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.getNumberOfRows(in: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath)
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SettingsViewController: UITableViewDelegate {
     
 }
