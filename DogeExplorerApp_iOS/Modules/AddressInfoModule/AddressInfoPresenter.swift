b680139bc991678363969a457442d25cd3a21307
032nnxkitty <aguznenkovv@yandex.ru>
2023-04-16 16:23:42 +0200

    Add services to the address info presenter

--- DogeExplorerApp_iOS/Modules/AddressInfoModule/AddressInfoPresenter.swift
+++ DogeExplorerApp_iOS/Modules/AddressInfoModule/AddressInfoPresenter.swift
@@ -16,14 +16,32 @@ final class AddressInfoPresenterImp: AddressInfoPresenter {
     private let networkManager: NetworkManager
     private let trackingService: AddressTrackingService
     
-    private var balanceModel: BalanceModel?
-    private var sentModel: SentModel?
-    private var receivedModel: ReceivedModel?
-    private var transactionsCountModel: TransactionsCountModel?
+    private let address: String
+    private var addressInfo: (BalanceModel, SentModel, ReceivedModel, TransactionsCountModel)?
     
     required init(address: String, view: AddressInfoView, networkManager: NetworkManager) {
         self.view = view
         self.networkManager = networkManager
+        self.address = address
         self.trackingService = UserDefaults.standard
+        initialize()
+    }
+}
+
+// MARK: - Private Methods
+private extension AddressInfoPresenterImp {
+    func initialize() {
+        Task {
+            do {
+                addressInfo = try await networkManager.getAddressInfo(address)
+                print(address)
+                print(addressInfo!.0.balance)
+                print(addressInfo!.1.sent)
+                print(addressInfo!.2.received)
+                print(addressInfo!.3.info.total)
+            } catch {
+                print("failure while loading address information")
+            }
+        }
     }
 }
