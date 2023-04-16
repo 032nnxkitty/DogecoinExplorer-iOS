f4f3c3f3ccefb2ab64c54763e2cffb63d73fcb9f
032nnxkitty <aguznenkovv@yandex.ru>
2023-04-16 16:24:21 +0200

    Set address info view title and background

--- DogeExplorerApp_iOS/Modules/AddressInfoModule/AddressInfoViewController.swift
+++ DogeExplorerApp_iOS/Modules/AddressInfoModule/AddressInfoViewController.swift
@@ -14,12 +14,23 @@ protocol AddressInfoView: AnyObject {
 final class AddressInfoViewController: UIViewController {
     public var presenter: AddressInfoPresenter?
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureViewAppearance()
     }
 }
 
+// MARK: - Private Methods
+private extension AddressInfoViewController {
+    func configureViewAppearance() {
+        view.backgroundColor = .systemBackground
+        navigationItem.largeTitleDisplayMode = .never
+        title = "Address"
+    }
+}
+
+// MARK: - AddressInfoView Protocol
 extension AddressInfoViewController: AddressInfoView {
     
 }
