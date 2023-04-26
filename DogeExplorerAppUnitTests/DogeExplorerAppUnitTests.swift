0f99702da444421d28b5e4d110525355a7864894
032nnxkitty <aguznenkovv@yandex.ru>
2023-04-26 18:34:21 +0200

    Add Unit and UI tests target

---
+++ DogeExplorerAppUnitTests/DogeExplorerAppUnitTests.swift
@@ -0,0 +1,29 @@
+//
+//  DogeExplorerAppUnitTests.swift
+//  DogeExplorerAppUnitTests
+//
+//  Created by Arseniy Zolotarev on 26.04.2023.
+//
+
+import XCTest
+
+final class DogeExplorerAppUnitTests: XCTestCase {
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
